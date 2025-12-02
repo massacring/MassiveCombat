2782a71c8e1df564cf0b6dee1ae3bf51e635449e
massacring <abbenylund@gmail.com>
2025-12-02 22:00:03 +0100

    Created base project.

---
+++ src/main/java/me/massacring/massiveCombat/MassiveCombat.java
@@ -0,0 +1,17 @@
+package me.massacring.massiveCombat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MassiveCombat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
