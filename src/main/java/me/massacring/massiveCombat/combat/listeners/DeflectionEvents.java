88f6b438bcb91970f98bf3ac7f4ae786dc65c9e2
massacring <abbenylund@gmail.com>
2026-01-22 21:05:46 +0100

    More stuff + prep.

--- src/main/java/me/massacring/massiveCombat/combat/listeners/DeflectionEvents.java
+++ src/main/java/me/massacring/massiveCombat/combat/listeners/DeflectionEvents.java
@@ -1,4 +1,4 @@
-package me.massacring.massiveCombat.deflection.listeners;
+package me.massacring.massiveCombat.combat.listeners;
 
 import me.deecaad.weaponmechanics.weapon.projectile.AProjectile;
 import me.deecaad.weaponmechanics.weapon.projectile.weaponprojectile.WeaponProjectile;
@@ -6,8 +6,8 @@ import me.deecaad.weaponmechanics.weapon.weaponevents.ProjectileHitEntityEvent;
 import me.massacring.massiveCombat.MassiveCombat;
 import org.bukkit.*;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -71,45 +71,42 @@ public class DeflectionEvents implements Listener {
         if (!(event.getEntity() instanceof Player player)) return;
         if (!player.hasPermission("massivecombat.ability.starter.deflect")) return;
         if (player.isBlocking()) return;
-        if (!(event.getDamager() instanceof AProjectile projectile)) return;
+        if (!(event.getDamager() instanceof Projectile projectile)) return;
+        player.sendMessage("Proj Class: " + projectile.getClass().getName());
 
-        // Check if the item in either hand can deflect.
-        ItemStack item = player.getInventory().getItemInMainHand();
-        if (!canDeflect(item)) {
-            item = player.getInventory().getItemInOffHand();
-            if (!canDeflect(item)) return;
-        }
+        if (!deflectCheck(player, projectile.getLocation().getDirection().normalize())) return;
 
-        // Check if the player is on cooldown
-        PersistentDataContainer playerNBT = player.getPersistentDataContainer();
-        if (playerNBT.has(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"))) {
-            Long cooldownTime = playerNBT.get(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"), PersistentDataType.LONG);
-            if (cooldownTime != null && System.currentTimeMillis() >= cooldownTime)
-                playerNBT.remove(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"));
-            else return;
-        }
+        player.sendMessage("boop");
 
-        // Check if the player is angled correctly
-        Vector playerDirection = player.getEyeLocation().getDirection().normalize();
-        Vector arrowDirection = projectile.getNormalizedMotion();
-        double dotProduct = Math.abs(playerDirection.dot(arrowDirection));
-        double minDot = Math.cos(Math.toRadians(this.minimumAngle));
-        if (dotProduct < minDot) {
-            // Cancel Shield Blocking
-            if (player.isBlocking() && event.getFinalDamage() == 0) {
-                player.damage(event.getDamage());
-                projectile.remove();
+        deflectAesthetics(player);
+
+        // Reverse the arrow's direction.
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                projectile.setVelocity(player.getEyeLocation().getDirection().multiply(power));
             }
-            return;
-        }
+        }.runTaskLater(this.plugin, 1);
 
-        // Get and play Sound
-        player.getWorld().playSound(player.getLocation(), this.sound, SoundCategory.PLAYERS, 1.0f, 1.0f);
+        event.setCancelled(true);
 
-        // Deflect particle
-        Vector playerVector = player.getLocation().getDirection().normalize();
-        Location sweepLocation = player.getEyeLocation().add(playerVector).subtract(new Vector(0, 0.3, 0));
-        player.getWorld().spawnParticle(Particle.SWEEP_ATTACK, sweepLocation, 1);
+        // set deflection cooldown tag
+        long cooldownTime = System.currentTimeMillis() + (this.useCooldown ? (this.cooldownTicks * 50L) : 0);
+        PersistentDataContainer playerNBT = player.getPersistentDataContainer();
+        playerNBT.set(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"), PersistentDataType.LONG, cooldownTime);
+    }
+
+    @EventHandler
+    public void deflectWMProjectile(ProjectileHitEntityEvent event) {
+        if (event.isCancelled()) return;
+        if (!(event.getEntity() instanceof Player player)) return;
+        if (!player.hasPermission("massivecombat.ability.starter.deflect")) return;
+        if (player.isBlocking()) return;
+        WeaponProjectile projectile = event.getProjectile();
+
+        if (!deflectCheck(player, projectile.getBukkitLocation().getDirection().normalize())) return;
+
+        deflectAesthetics(player);
 
         // Reverse the arrow's direction.
         new BukkitRunnable() {
@@ -123,6 +120,44 @@ public class DeflectionEvents implements Listener {
 
         // set deflection cooldown tag
         long cooldownTime = System.currentTimeMillis() + (this.useCooldown ? (this.cooldownTicks * 50L) : 0);
+        PersistentDataContainer playerNBT = player.getPersistentDataContainer();
         playerNBT.set(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"), PersistentDataType.LONG, cooldownTime);
+    }
+
+    private boolean deflectCheck(Player player, Vector projDirection) {
+        // Check if the item in either hand can deflect.
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (!canDeflect(item)) {
+            item = player.getInventory().getItemInOffHand();
+            if (!canDeflect(item)) return false;
+        }
+
+
+        // Check if the player is on cooldown
+        PersistentDataContainer playerNBT = player.getPersistentDataContainer();
+        if (playerNBT.has(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"))) {
+            Long cooldownTime = playerNBT.get(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"), PersistentDataType.LONG);
+            if (cooldownTime != null && System.currentTimeMillis() >= cooldownTime)
+                playerNBT.remove(new NamespacedKey(this.plugin, "massivecombat.deflect.cooldown"));
+            else return false;
+        }
+
+        // Check if the player is angled correctly
+        Vector playerDirection = player.getEyeLocation().getDirection().normalize();
+        double dotProduct = Math.abs(playerDirection.dot(projDirection));
+        double minDot = Math.cos(Math.toRadians(this.minimumAngle));
+        if (dotProduct < minDot) return false;
+
+        return true;
+    }
+
+    private void deflectAesthetics(Player player) {
+        // Get and play Sound
+        player.getWorld().playSound(player.getLocation(), this.sound, SoundCategory.PLAYERS, 1.0f, 1.0f);
+
+        // Deflect particle
+        Vector playerVector = player.getLocation().getDirection().normalize();
+        Location sweepLocation = player.getEyeLocation().add(playerVector).subtract(new Vector(0, 0.3, 0));
+        player.getWorld().spawnParticle(Particle.SWEEP_ATTACK, sweepLocation, 1);
     }
 }
