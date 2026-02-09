66672e773903d55e94a60db0312ca283a270e178
massacring <abbenylund@gmail.com>
2026-02-09 15:31:27 +0100

    Renamed placeholders.

--- src/main/java/me/massacring/massiveCombat/combat/listeners/WeaponMechanicsEvents.java
+++ src/main/java/me/massacring/massiveCombat/combat/listeners/WeaponMechanicsEvents.java
@@ -13,10 +13,10 @@ public class WeaponMechanicsEvents implements Listener {
     public void weaponDamageEvent(WeaponDamageEntityEvent event) {
         if (!(event.getSource().getShooter() instanceof Player player)) return;
         double blasterDamage = Double.parseDouble(
-                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_blaster_damage%")
+                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_BLASTER_FLAT_DMG%")
         );
         double blasterDamagePercent = Double.parseDouble(
-                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_blaster_damage_percent%")
+                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_BLASTER_PERCENT_DMG%")
         );
         double baseDamage = event.getBaseDamage() + blasterDamage;
         double newDamage = baseDamage + (baseDamage * blasterDamagePercent/100);
@@ -27,10 +27,10 @@ public class WeaponMechanicsEvents implements Listener {
     public void weaponReloadEvent(WeaponReloadEvent event) {
         if (!(event.getEntity() instanceof Player player)) return;
         double blasterReload = Double.parseDouble(
-                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_blaster_reload_reduction%")
+                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_BLASTER_FLAT_RELOAD_RDC%")
         );
         double blasterReloadPercent = Double.parseDouble(
-                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_blaster_reload_reduction_percent%")
+                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_BLASTER_PERCENT_RELOAD_RDC%")
         );
         double baseReload = event.getReloadTime() - blasterReload;
         double newReload = baseReload - (baseReload * blasterReloadPercent/100);
@@ -41,7 +41,7 @@ public class WeaponMechanicsEvents implements Listener {
     public void weaponShootEvent(PrepareWeaponShootEvent event) {
         if (!(event.getShooter() instanceof Player player)) return;
         double blasterSpread = Double.parseDouble(
-                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_blaster_spread_reduction%")
+                PlaceholderAPI.setPlaceholders(player, "%mmocore_stat_BLASTER_PERCENT_SPREAD_RDC%")
         );
         double baseSpread = event.getBaseSpread();
         double newSpread = baseSpread - (baseSpread * (blasterSpread/100));
